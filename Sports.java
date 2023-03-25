4cb2195a1dc34b6e1acb7207d25f288d9ce15be1
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-25 09:43:55 +0530

    Update Sports.java

--- Sports.java
+++ Sports.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 class Sports{
     String getName(){
@@ -19,3 +18,29 @@ class Soccer extends Sports{
         System.out.println( "Each team has 11 players in " + getName() );
     }
 }
+
+Main Function:
+
+public class Solution {
+    public static void main(String[] args) {
+        Sports s;
+        s=new Sports();
+        System.out.println(""+s.getName());
+        s.getNumberOfTeamMembers();
+        s=new Soccer();
+        System.out.println(""+s.getName());
+        s.getNumberOfTeamMembers();
+    }
+}
+
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=5243:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Inheritence\out\production\Inheritence Solution
+
+Generic Sports
+Each team has n players in Generic Sports
+Soccer class.
+Each team has 11 players in Soccer class.
+
+Process finished with exit code 0
