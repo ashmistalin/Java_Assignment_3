4c738329fd54f97dfd4c7743a537601c0fee9b1d
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-25 09:41:00 +0530

    Add files via upload

---
+++ Sports.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Sports{
+    String getName(){
+        return "Generic Sports";
+    }
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer class.";
+    }
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
